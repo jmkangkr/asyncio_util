17cbbb1207d879df72ea5005889a67768da44b3c
Jaemin Kang <jmkangkr@gmail.com>
2019-04-12 17:27:10 +0900

    Print error message when there's an exception

--- asyncio_timer_callback.py
+++ asyncio_timer_callback.py
@@ -1,26 +1,39 @@
 import asyncio
 import inspect
+import logging
+import traceback
 
 
 class AsyncioTimerCallback:
-    def __init__(self, timeout, callback, args=()):
+    def __init__(self, logger_name, timeout, callback, args=()):
+        if logger_name:
+            self._logger = logging.getLogger(logger_name)
+        else:
+            self._logger = None
+
         self._timeout = timeout
         self._callback = callback
         self._args = args
         self._task = None
 
     def start(self):
-        self._task = asyncio.ensure_future(self._job(self._timeout, self._callback, self._args))
+        self._task = asyncio.ensure_future(self._job(self._timeout, self._callback, self._args, self._logger))
 
         return self
 
     @staticmethod
-    async def _job(timeout, callback, args):
+    async def _job(timeout, callback, args, logger):
         await asyncio.sleep(timeout)
         if inspect.iscoroutinefunction(callback):
             await callback(*args)
         else:
-            callback(*args)
+            try:
+                callback(*args)
+            except Exception as e:
+                if logger:
+                    logger.info("Exception occured in AsyncioTimerCallback job: {}\n{}".format(str(e), traceback.format_exc()))
+                else:
+                    print("Exception occured in AsyncioTimerCallback job: {}\n{}".format(str(e), traceback.format_exc()))
 
     def cancel(self):
         if self._task:
